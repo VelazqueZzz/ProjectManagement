b18a6493dcea2b6dc3b9f12e7f20c7aebbb00f5b
VelazqueZzz <93205261+VelazqueZzz@users.noreply.github.com>
2024-05-22 23:56:19 +0300

    Add files via upload

---
+++ view/task/TaskDetailView.java
@@ -0,0 +1,15 @@
+package com.company.projectmanagement.view.task;
+
+
+import com.company.projectmanagement.entity.Task;
+import com.company.projectmanagement.view.main.MainView;
+
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "tasks/:id", layout = MainView.class)
+@ViewController("Task_.detail")
+@ViewDescriptor("task-detail-view.xml")
+@EditedEntityContainer("taskDc")
+public class TaskDetailView extends StandardDetailView<Task> {
+}
